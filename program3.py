d5fcee7dbe7abadc138c009c8e02c4b13cf663e7
jasminericka <jasmin.celebre@gmail.com>
2023-04-28 20:49:20 +0800

    set line conditions

--- program3.py
+++ program3.py
@@ -8,3 +8,12 @@ def line_write():
 #ask for user input of line
         line = input("Enter line:")
         my_file.write(line + "\n")
+#set a conditon for lines
+        while True:
+            answer = input("Are there more lines? y/n:")
+            if answer == "y":
+                line = input("enter line:")
+                my_file.write(line + "\n")
+            else:
+                break
+line_write()
